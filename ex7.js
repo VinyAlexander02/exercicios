c06eddb2281b491a001a3c3c7d7e4cdfe1d9af54
Vinicius Alexander <vinicius.malex2002@gmail.com>
2020-08-24 21:55:38 -0300

    Exercicio 7 modificado

---
+++ ex7.js
@@ -0,0 +1,19 @@
+function chefe() {
+    ex7()
+}
+
+function ex7() {
+
+    var soma = 0
+    var conta = 0
+    var num = Number(prompt("Informe um número"))
+    do {
+        conta++ // soma1
+        soma = soma + num // acumulador 
+
+        num = Number(prompt("Informe outro número ou Digite negativo para finalizar"))
+    }
+    while(num >= 0)
+
+    console.log(`A média dos números é ${soma / conta}`)
+}
